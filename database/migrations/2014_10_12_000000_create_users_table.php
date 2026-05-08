78bcc0d988cc80f7d05718ceca87d3df8622a990
dhabyap <dhabyap@gmail.com>
2026-05-08 11:44:18 +0700

    feat: create users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('full_name');
+            $table->string('phone_number')->nullable();
+            $table->boolean('is_verified')->default(false);
+            $table->string('verification_code')->nullable();
+            $table->timestamp('last_login_at')->nullable();
             $table->timestamps();
         });
     }
